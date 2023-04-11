c6e6509cfe87f0e85a324b00f83bf926387b30ef
Maya <pchelaamaya@gmail.com>
2023-04-11 16:06:48 +0300

    add costItem and costDate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import CostItem from "./components/CostItem";
 
 function App() {
+
+  const costs = [
+    {
+      date: new Date(2022, 8, 10),
+      description: "Холодильник",
+      amount: 999.99
+    },
+    {
+      date: new Date(2022, 8, 12),
+      description: "Плита",
+      amount: 799.99
+    },
+    {
+      date: new Date(2022, 9, 14),
+      description: "Стиральная машина",
+      amount: 899.99
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title">Учёт трат</h1>
+      <CostItem 
+        date={costs[0].date} 
+        description={costs[0].description} 
+        amount={costs[0].amount}
+      />
+      <CostItem
+        date={costs[1].date} 
+        description={costs[1].description} 
+        amount={costs[1].amount}
+      />
+      <CostItem
+        date={costs[2].date} 
+        description={costs[2].description} 
+        amount={costs[2].amount}
+      />
     </div>
   );
 }
