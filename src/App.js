fd7ac9708ef1fb44a0334479327e50fcce362b8f
Maya <pchelaamaya@gmail.com>
2023-04-13 13:10:26 +0300

    add parameters of input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import NewCost from "./components/NewCost/NewCost";
 import Costs from "./components/Costs/Costs";
 const App = () => {
   const costs = [
     {
+      id: 'c1',
       date: new Date(2022, 8, 10),
       description: "Холодильник",
       amount: 999.99
     },
     {
+      id: 'c2',
       date: new Date(2022, 8, 12),
       description: "Плита",
       amount: 799.99
     },
     {
+      id: 'c3',
       date: new Date(2022, 9, 14),
       description: "Стиральная машина",
       amount: 899.99
     },
   ];
+
+  const addCostHandler = (cost) => {
+
+  }
+
   return (
     <div>
-      <h1 className="title">Учёт трат</h1>
+      <NewCost onAddCost={addCostHandler}/>
       <Costs costs={costs}/>
     </div>
   );
---
+++ src/components/NewCost/CostForm.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react'
+import './CostForm.css'
+const CostForm = (props) => {
+
+    const [inputName, setInputName] = useState('');
+    const [inputAmount, setInputAmount] = useState('');
+    const [inputDate, setInputDate] = useState('');
+
+    //Лучше использовать следующий способ передачи состояния Input,
+    //но в данном случае используются стр.функции ниже через несколько состояний
+    // const nameChangeHandler = (event) => {
+    //     setUserInput((previousState) => {
+    //         return {
+    //             ...previousState,
+    //             name: event.target.value 
+    //         }
+    //     })
+    // }
+
+    const nameChangeHandler = (event) => {
+        setInputName(event.target.value);
+    }
+    const amountChangeHandler = (event) => {
+        setInputAmount(event.target.value);
+    }
+    const dateChangeHandler = (event) => {
+        setInputDate(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const costData = {
+            name: inputName,
+            amount: inputAmount,
+            date: new Date(inputDate)
+        }
+        //ниже коммуникация с компонентом NewCost
+        props.onSaveCostData();
+        setInputName('');
+        setInputAmount('');
+        setInputDate('');
+
+    };
+    return (
+        <form onSubmit={submitHandler}>
+            <div className='new-cost__controls'>
+                <div className='new-cost__control'>
+                    <label>Название</label>
+                    <input
+                     type='text'
+                     value={inputName} 
+                     onChange={nameChangeHandler}
+                    />
+                </div> 
+                <div className='new-cost__control'>
+                    <label>Сумма</label>
+                    <input
+                     type='number' 
+                     value={inputAmount}
+                     onChange={amountChangeHandler} 
+                     min='0.01' 
+                     step='0.01' 
+                    />
+                </div> 
+                <div className='new-cost__control'>
+                    <label>Дата</label>
+                    <input
+                     type='date' 
+                     value={inputDate}
+                     onChange={dateChangeHandler} 
+                     min='2019-01-01' 
+                     step='2023-12-31'
+                    />
+                </div> 
+                <div className='new-cost__actions'>
+                    <button type='submit'>Добавить расход</button>
+                </div>
+            </div>
+        </form>
+        )
+}
+
+export default CostForm;
